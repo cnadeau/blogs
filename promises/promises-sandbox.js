6d5a205b7b47f9ff9923355efb8e3acc94455f44
Christian Nadeau <cnadeau@macadamian.com>
2017-02-10 11:14:25 -0500

    Added sandbox to promises

---
+++ promises/promises-sandbox.js
@@ -0,0 +1,36 @@
+function handleThen(id) {
+  console.log(`then ${id}`);
+
+  // Returns an object with throwError function
+  // to be able to throw a new error in a then
+  return {
+    throwError: () => {
+      throw new Error(`error ${id}`);
+    },
+    reject: () => Promise.reject(new Error(`error ${id}`))
+  };
+}
+
+function handleCatch(id, error) {
+  console.log(`catch ${id} : ${error}`);
+
+  // Returns an object with rethrow function
+  // to be able to rethrow the received error
+  // in a catch
+  return {
+    rethrow: () => {
+      throw error;
+    },
+    reject: () => Promise.reject(error)
+  };
+}
+
+Promise.resolve()
+  .then(() => handleThen(1))
+  .catch(e => handleCatch(1, e))
+  .then(() => handleThen(2))
+  .catch(e => handleCatch(2, e))
+  .then(() => handleThen(3))
+  .catch(e => handleCatch(3, e))
+  .catch(e => console.log(`something went wrong : ${e}`))
+  .then(() => console.log('Done'));
